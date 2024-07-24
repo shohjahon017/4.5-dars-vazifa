646b6bd0621a17f1425310109ac1446dbcde4d74
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-24 15:59:31 +0500

    .

--- js/script.js
+++ js/script.js
@@ -5,12 +5,10 @@
 //   double.push(arr[i] * 2);
 // }
 // console.log(double);
-
 //2-masala
 // let arr = ["salom", 121, 8n, 23, "bye"];
 // let res = arr.reverse();
 // console.log(res);
-
 //3-masala
 // let arr = [-15, 5, 6, 89, -5, -14];
 // let newArr = [];
@@ -20,7 +18,6 @@
 //   }
 // }
 // console.log(newArr);
-
 //4-masala
 // let sum = 0;
 // let arr = [4, 5, 3, 6, 4, 9, 15, 25];
@@ -28,32 +25,26 @@
 //   sum += arr[i];
 // }
 // console.log(sum);
-
 //5-masala
 // let arr = [15, 2, 6, 15, 85, 4, "xayr"];
 // arr.push("salom");
 // console.log(arr);
-
 //6-masala
 // let arr = [15, 5, 3, 6, 8, true, "salom", 25, 6n];
 // arr.pop();
 // console.log(arr);
-
 //7-masala
 // let arr = [11, 155, 3, 5, 6, 25, 5];
 // arr.shift();
 // console.log(arr);
-
 //8-masala
 // let arr = [45, "xayr", 56, true, 523];
 // arr.unshift("xayr");
 // console.log(arr);
-
 //9-masala
 // let arr = [14, 22, 6526, "salom", 256, "xayr"];
 // let res = arr.indexOf("salom");
 // console.log(res);
-
 //10-masala
 // let arr = [14, 25, 3, 6, 8, 9];
 // let newArr = [];
@@ -61,7 +52,6 @@
 //   newArr.push(arr[i]);
 // }
 // console.log(newArr);
-
 //11-masala
 // let arr = [2, 5, 3, 6, 56, 9, 59, 5];
 // let max = arr[0];
@@ -71,7 +61,6 @@
 //   }
 // }
 // console.log(max);
-
 //12-masala
 // let arr = [2, 5, 6, 59, 155, 3, 5];
 // let min = arr[0];
@@ -81,6 +70,90 @@
 //   }
 // }
 // console.log(min);
+//13-masala
+// let count = 0;
+// let arr = [1, 5, 4, 8, 10, 9, 12, 14];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     count++;
+//   }
+// }
+// console.log(count);
+//14-masala
+// let count = 0;
+// let arr = [15, 26, 15, 95, 30, 14, 65, 11, 22, 13];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 1) {
+//     count++;
+//   }
+// }
+// console.log(count);
+//15-masala
+// let arr = [1, 25, 3, 0, 35, 26, 0, 6];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] != 0) {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
+//16-masala
+// let arr = [2, 5, 6, 7];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   newArr.push(arr[i], arr[i]);
+// }
+// console.log(newArr);
+//17-masala
+// let arr = [14, 5, 2, 66, 62, 65, 1];
+
+//18-masala
+// let arr = [1, 4, 2, 3, 6];
+// let p = 1;
+// for (let i = 0; i < arr.length; i++) {
+//   p *= arr[i];
+// }
+// console.log(p)
+
+//19-masala
+// let arr = [14, 2, 3, 6, 4, 5, 8, 9];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (i % 2 !== 0) {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
+
+//20-masala
+// let arr = [1, 2, 5, 6, 41, 55, 84, 7];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (i % 2 == 0) {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
+
+//21-masala
+// let arr = [1, 515, "salom", 36, 85, "bye", 11];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (typeof arr[i] == "string") {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
+
+//22-masala
+// let arr = [1, 515, "salom", 36, 85, "bye", 11];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (typeof arr[i] == "number") {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
 
 //23-masala
 // let arr = [5, 12, 26, 3, 6,  48, 9, 10];
