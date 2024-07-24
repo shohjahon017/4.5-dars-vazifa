20faf028d5e18916e5119f2c0240842785fad758
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-24 14:56:57 +0500

    ..

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//1-masala
+// let arr = [5, 4, 6, 9, 8, 32, 14];
+// let double = [];
+// for (let i = 0; i < arr.length; i++) {
+//   double.push(arr[i] * 2);
+// }
+// console.log(double);
+
+//2-masala
+// let arr = ["salom", 121, 8n, 23, "bye"];
+// let res = arr.reverse();
+// console.log(res);
+
+//3-masala
+// let arr = [-15, 5, 6, 89, -5, -14];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] >= 0) {
+//     newArr.push(arr[i]);
+//   }
+// }
+// console.log(newArr);
+
+//23-masala
+// let arr = [5, 12, 26, 3, 6,  48, 9, 10];
+// console.log(arr.length);
