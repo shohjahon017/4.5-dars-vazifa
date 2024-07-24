d3bbc622123da0ef2c5c4bce3b9a5a9dd46701c1
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-24 15:25:09 +0500

    .

--- js/script.js
+++ js/script.js
@@ -30,9 +30,57 @@
 // console.log(sum);
 
 //5-masala
-let arr = [15, 2, 6, 15, 85, 4, "xayr"];
-arr.push("salom");
-console.log(arr);
+// let arr = [15, 2, 6, 15, 85, 4, "xayr"];
+// arr.push("salom");
+// console.log(arr);
+
+//6-masala
+// let arr = [15, 5, 3, 6, 8, true, "salom", 25, 6n];
+// arr.pop();
+// console.log(arr);
+
+//7-masala
+// let arr = [11, 155, 3, 5, 6, 25, 5];
+// arr.shift();
+// console.log(arr);
+
+//8-masala
+// let arr = [45, "xayr", 56, true, 523];
+// arr.unshift("xayr");
+// console.log(arr);
+
+//9-masala
+// let arr = [14, 22, 6526, "salom", 256, "xayr"];
+// let res = arr.indexOf("salom");
+// console.log(res);
+
+//10-masala
+// let arr = [14, 25, 3, 6, 8, 9];
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   newArr.push(arr[i]);
+// }
+// console.log(newArr);
+
+//11-masala
+// let arr = [2, 5, 3, 6, 56, 9, 59, 5];
+// let max = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
+
+//12-masala
+// let arr = [2, 5, 6, 59, 155, 3, 5];
+// let min = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+// }
+// console.log(min);
 
 //23-masala
 // let arr = [5, 12, 26, 3, 6,  48, 9, 10];
